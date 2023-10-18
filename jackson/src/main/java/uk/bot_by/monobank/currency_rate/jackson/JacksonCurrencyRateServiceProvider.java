0be72c488af21dd8f1727cf5b0e0d8ec44e1e359
Vitalij Berdinskih <vitalij_r2@outlook.com>
2023-10-19 01:04:03 +0300

    Update Javadoc, rename folders, optimize dependencies

---
+++ jackson/src/main/java/uk/bot_by/monobank/currency_rate/jackson/JacksonCurrencyRateServiceProvider.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Vitalij Berdinskih
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.bot_by.monobank.currency_rate.jackson;
+
+import feign.codec.Decoder;
+import feign.jackson.JacksonDecoder;
+import org.jetbrains.annotations.VisibleForTesting;
+import uk.bot_by.monobank.currency_rate.CurrencyRateService;
+import uk.bot_by.monobank.currency_rate.CurrencyRateServiceProvider;
+
+/**
+ * A provider creates a service with the {@linkplain JacksonDecoder Gson decoder}.
+ */
+public class JacksonCurrencyRateServiceProvider extends CurrencyRateServiceProvider {
+
+  public JacksonCurrencyRateServiceProvider() {
+    super();
+  }
+
+  @VisibleForTesting
+  JacksonCurrencyRateServiceProvider(String locator) {
+    super(locator);
+  }
+
+  @Override
+  protected Decoder getDecoder() {
+    return new JacksonDecoder();
+  }
+
+  @Override
+  protected Class<? extends CurrencyRateService> getServiceType() {
+    return JacksonCurrencyRateService.class;
+  }
+
+}
